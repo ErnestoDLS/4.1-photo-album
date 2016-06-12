01e91cb90529527f867387cbddce74a899a0ba0b
Ernesto De Los Santos <netos_id@yahoo.com>
2016-06-12 18:05:23 -0500

    does not render but no errors

--- js/bundle.js
+++ js/bundle.js
@@ -1,3 +1,21 @@
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+var $ = require("jquery");
+var modal = require("modal");
+
+$(function(){
+  console.log("DOM READY!S");
+  modal.toggleShow("[data-js='modal']");
+});
+
+},{"jquery":3,"modal":2}],2:[function(require,module,exports){
+var $ = require("jquery");
+
+this.toggleShow = function(selector) {
+  $(selector).toggleClass("modal--hide");
+  console.log("MODAL TOGGLED");
+}
+
+},{"jquery":3}],3:[function(require,module,exports){
 /*!
  * jQuery JavaScript Library v2.2.3
  * http://jquery.com/
@@ -9840,3 +9858,5 @@ if ( !noGlobal ) {
 
 return jQuery;
 }));
+
+},{}]},{},[1]);
--- js/main.js
+++ js/main.js
@@ -1,29 +1,60 @@
 $(function(){
-  // Code here!
 
-  var imagesArray = [
+  var albumOne = [
     {
-      title: "couple"
+      title: "1",
       src: "images/couple.jpeg"
     },
     {
-      title: "hands"
-      src:"images/fathertime.jpeg"
+      title: "2",
+      src: "images/yellowbuilding.jpeg"
     },
     {
-      title: "buliding"
-      src:"images/yellowbuilding.jpeg"
+      title: "3",
+      src: "images/squad.jpg"
     }
   ]
-})
-  var $mainElement = ("[data-js='main']");
-  var $modalElement = ("[data-js='modal']");
 
-  imagesArray.forEach(function(images){
-    var newImage = $("<img>").attr("src", images.src);
-    $mainElement.append(newImage);
+  var $photoList = $("[data-js='photo__list']");
+  var $modalCouple = $("[data-js='modal__couple']");
+  var $modalBuilding = $("[data-js='modal__building']");
+  var $modalDogs = $("[data-js='modal__dogs']");
+  var $backButton = $("[data-js='back__button']");
 
-    $(document).ready(function(){
-      $('.thumb-nailcontainer').thumb-nailcontainer({width:100, height:100 });
-    });
+  albumOne.forEach(function(image){
+
+    var imageContainer = $(`<li class= 'image__container'>${image.title}</li>`);
+
+    var newImage = $("<img class='image'>").attr("src", image.src);
+
+    imageContainer.append(newImage);
+
+    $photoList.append(imageContainer);
+  });
+
+
+  $photoList.on("click", "li:first-child", function(){
+    $modalCouple.show("modal__hide--couple")
   })
+
+  $photoList.on("click", "li:nth-child(2)", function(){
+    $modalBuilding.show("modal__hide--building")
+  })
+
+  $photoList.on("click", "li:nth-child(3)", function(){
+    $modalDogs.show("modal__hide--dogs")
+  })
+
+  $backButton.on("click", function(){
+    $modalCouple.hide("modal__hide--couple")
+  })
+
+  $backButton.on("click", function(){
+    $modalBuilding.hide("modal__hide--building")
+  })
+
+  $backButton.on("click", function(){
+    $modalDogs.hide("modal__hide--dogs")
+  })
+
+});
